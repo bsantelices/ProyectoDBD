8e03110eee43a464c9d2918fe3ac9ed3b1986d4d
Unknown <bryan.santelices@usach.cl>
2018-12-11 16:34:47 -0300

    Levantando Proyecto

---
+++ lab_dbd/app/Http/Controllers/HoliController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\holi;
+use Illuminate\Http\Request;
+
+class HoliController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\holi  $holi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(holi $holi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\holi  $holi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(holi $holi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\holi  $holi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, holi $holi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\holi  $holi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(holi $holi)
+    {
+        //
+    }
+}
