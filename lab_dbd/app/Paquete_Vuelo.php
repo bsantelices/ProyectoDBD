494702dd61f310a44976b937485369b3f6415b43
Matias Coronado <matias.coronado@usach.cl>
2018-12-19 02:58:30 -0300

    Actualización restante

--- lab_dbd/app/Paquete_Vuelo.php
+++ lab_dbd/app/Paquete_Vuelo.php
@@ -6,7 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Paquete_Vuelo extends Model
 {
-    protected $fillable = [
-        'monto','descripcion','fecha_reserva',
-    ];
+	//
 }
--- lab_dbd/app/Reserva.php
+++ lab_dbd/app/Reserva.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reserva extends Model
 {
-	protected $fillable = [
-        'monto','descripcion','fecha_compra','hora_compra','correo',
+    protected $fillable = [
+        'monto','descripcion','fecha_reserva',
     ];
 }
