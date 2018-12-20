f83f499244d56732b0f2b0c2c9a96171aa4cf00d
Unknown <bryan.santelices@usach.cl>
2018-12-19 23:12:01 -0300

    Inicio de las relaciones

--- lab_dbd/app/Airport.php
+++ lab_dbd/app/Airport.php
@@ -6,6 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Airport extends Model
 {
+	public function location()
+    {
+        return $this->hasOne(Location::class,'location_id');
+    }	
+    public function plane()
+    {
+        return $this->hasMany(Plane::class,'plane_id');
+    }
+
+    public function flight()
+    {
+        return $this->hasOne(Flight::class,'flight_id');
+    }
+
+
     protected $fillable = [
         'names','type'
     ];
--- lab_dbd/app/Discount.php
+++ lab_dbd/app/Discount.php
@@ -5,7 +5,12 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Discount extends Model
-{
+{	
+	public function package()
+    {
+        return $this->hasMany(Package::class,'package_id');
+    }
+
     protected $fillable = [
         'amount','type','title','description'
     ];
