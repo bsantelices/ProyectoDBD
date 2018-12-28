09663910257eecbf603ad1f1e9d5486adc2cbcbb
Unknown <bryan.santelices@usach.cl>
2018-12-28 00:24:26 -0300

    Fix some factorys and create some files for next seeds

--- lab_dbd/database/factories/LocationFactory.php
+++ lab_dbd/database/factories/LocationFactory.php
@@ -4,9 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Location::class, function (Faker $faker) {
     return [
-        'coordinates' => 'test',
-        'city' => 'test',
-        'country' => 'test',
-        'postalCode' => 'test'
+        'coordinates' => $faker->text,
+        'city' => $faker->city,
+        'country' => $faker->country,
+        'street' => $faker->streetName,
+        'houseCode' =>$faker->buildingNumber,
+        'postalCode' => $faker->postcode,
     ];
 });
