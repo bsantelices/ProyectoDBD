3e8310924bc87a0c598392aa9ec20825041a1ea4
Matias Coronado <matias.coronado@usach.cl>
2018-12-28 00:59:38 -0300

    Reservation factory fix

--- lab_dbd/database/factories/ReservationFactory.php
+++ lab_dbd/database/factories/ReservationFactory.php
@@ -4,10 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Reservation::class, function (Faker $faker) {
     return [
-        'amount',
-        'description',
-        'completed',
-        'payment_method_id',
-        'user_id'
+        'amount'=>$faker->numberBetween($min = 1000, $max = 500000),
+        'description'=>$faker->realText($maxNbChars = 50, $indexSize = 2),
+        'completed'=>$faker->boolean,
+        'payment_method_id'=>$faker->unique->numberBetween($min = 0, $max = 50),
+        'user_id'=>$faker->unique->numberBetween($min = 0, $max = 50)
     ];
 });
