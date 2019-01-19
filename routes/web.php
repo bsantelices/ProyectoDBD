224771b87b4c8b61a533157d89e6491738ffcf45
dannyandres <danny.aguilar@usach.cl>
2019-01-19 11:02:28 -0300

    nested routes

--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('airports','AirportController');
-Route::resource('discounts','DiscountController');
-Route::resource('flights','FlightController');
-Route::resource('hotels','HotelController');
-Route::resource('insurances','InsuranceController');
-Route::resource('locations','LocationController');
-Route::resource('logs','LogController');
-Route::resource('packages','PackageController');
-Route::resource('paymentMethods','PaymentMethodController');
-Route::resource('planes','PlaneController');
-Route::resource('reservations','ReservationController');
-Route::resource('roles','RoleController');
-Route::resource('rooms','RoomController');
-Route::resource('seats','SeatController');
-Route::resource('users','UserController');
-Route::resource('vehicles','VehicleController');
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+// Route::resource('airports','AirportController');
+// Route::resource('discounts','DiscountController');
+// Route::resource('flights','FlightController');
+// Route::resource('hotels','HotelController');
+// Route::resource('insurances','InsuranceController');
+// Route::resource('locations','LocationController');
+// Route::resource('logs','LogController');
+// Route::resource('packages','PackageController');
+// Route::resource('paymentMethods','PaymentMethodController');
+// Route::resource('planes','PlaneController');
+//Route::resource('reservations','ReservationController');
+// Route::resource('roles','RoleController');
+// Route::resource('rooms','RoomController');
+// Route::resource('seats','SeatController');
+//Route::resource('users','UserController');
+// Route::resource('vehicles','VehicleController');
+
+
+
+Route::resource('users.reservations','User\ReservationController');
