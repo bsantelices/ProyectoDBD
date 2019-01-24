88d208f8bd4d347f96d370f1c9d1d34214de02c3
Matias <matias.coronado@usach.cl>
2019-01-24 18:03:03 -0300

    Search of rooms, by filters
    This is the advance of the other day.

--- resources/views/room/index.blade.php
+++ resources/views/room/index.blade.php
@@ -8,28 +8,28 @@
 			{{ csrf_field() }}
 			<div class="form-group">
 				<label >Pais</label>
-				<select name="country"  id="pais" class="selectpicker">
+				<select name="country"  id="country" class="selectpicker" value = "{{old('country')}}">
 					@foreach($locations as $location)
-						<option value="{{$location['id']}}">{{$location['country']}}</option>
+						<option value="{{$location['country']}}">{{$location['country']}}</option>
 					@endforeach
 				</select>			
 
 				<label >Ciudad</label>
 				<select name="city" id="ciudad" class="selectpicker">
 					@foreach($locations as $location)
-						<option value="{{$location['id']}}">{{$location['city']}}</option>
+						<option value="{{$location['city']}}">{{$location['city']}}</option>
 					@endforeach
 				</select>
 			
-				<label for="selectTypeRoom">Tipo de habitacion</label>
-				<select class="form-control" id="selectTypeRoom" name = "typeRoom">
-					  <option>basic</option>
-					  <option>vip</option>
-					  <option>premium</option>
+				<label for="typeRoom">Tipo de habitacion</label>
+				<select class="form-control" id="typeRoom" name = "typeRoom">
+					  <option value ="basic">Basico</option>
+					  <option value = "vip">Vip</option>
+					  <option value = "premium">Premium</option>
 				</select>
 
-				<label for="selectTypeRoom">Capacidad de Adultos</label>
-				<select class="form-control" id="selectTypeRoom" name = "adultCapacity">
+				<label for="adultCapacity">Capacidad de Adultos</label>
+				<select class="form-control" id="adultCapacity" name = "adultCapacity">
 					  <option>1</option>
 					  <option>2</option>
 					  <option>3</option>
@@ -53,13 +53,7 @@
 					  <option>3</option>
 					  <option>4</option>
 					  <option>5</option>
-				</select>		
-
-				<label for="selectTypeRoom">Orden de precios</label>
-				<select class="form-control" id="selectTypeRoom" name = "order">
-					  <option>Mayor precio primero</option>
-					  <option>Menor precio primero</option>
-				</select>	
+				</select>						
 
 			<div class ="field">
 					<div class = control>
@@ -70,19 +64,14 @@
 			</div>
 		</form>
 
-		<table class="table">
-		    <thead>
-		      <tr>
-		        <th>Tipo habitacion</th>
-		      </tr>	
-		 	</thead>
-		@foreach($rooms as $room)  
-		    <tbody>
-		    	<tr>{{$room->type}}
-		    	</tr>
-		    </tbody>
-		</table>
-		@endforeach
+		@foreach($rooms as $room)
+			<?php 	$hotel = App\Hotel::find($room->hotel_id);
+					$location = App\Location::find($hotel->location_id);
+			?>
+				<h1>{{$room['type']}}</h1>
+		@endforeach			
+	 
+		
 
 	</div>
 
